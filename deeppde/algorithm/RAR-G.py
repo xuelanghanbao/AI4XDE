4aafbdd4635cceb9ad488d54d969e413a8d99a02
xuelanghanbao <710616871@qq.com>
2023-04-25 13:21:54 +0800

    取消epoch参数

--- deeppde/algorithm/RAR-G.py
+++ deeppde/algorithm/RAR-G.py
@@ -3,19 +3,22 @@ import numpy as np
 from solver.PDESolver import PINNSolver
 
 class RAR_G(PINNSolver):
-    def __init__(self, case='Burgers', NumDomain=1000, epoch=100):
-        super().__init__(name='RAR_G', case=case, NumDomain=NumDomain, epoch=epoch)
+    def __init__(self, case='Burgers', NumDomain=1000):
+        super().__init__(name='RAR_G', case=case, NumDomain=NumDomain)
 
     def closure(self):
-        X = self.geomtime.random_points(100000)
-        Y = np.abs(self.model.predict(X, operator=self.pde))[:, 0]
-        err_eq = torch.tensor(Y)
-        X_ids = torch.topk(err_eq, self.NumDomain//100, dim=0)[1].cpu().numpy()
-        self.data.add_anchors(X[X_ids])
+        self.train_step()
 
-        self.train_step(iteration=1000)
+        for i in range(100):
+            X = self.geomtime.random_points(100000)
+            Y = np.abs(self.model.predict(X, operator=self.pde))[:, 0]
+            err_eq = torch.tensor(Y)
+            X_ids = torch.topk(err_eq, self.NumDomain//100, dim=0)[1].cpu().numpy()
+            self.data.add_anchors(X[X_ids])
+
+            self.train_step(iteration=1000)
 
 if __name__ == '__main__':
-    solver = RAR_G(case='Burgers', NumDomain=2000//2, epoch=100)
+    solver = RAR_G(case='Burgers', NumDomain=2000//2)
     solver.train()
     solver.save(add_time=True)
