751356ae45bae82d2975fa274e73f9c5d869dee5
xuelanghanbao <710616871@qq.com>
2023-04-25 22:03:49 +0800

    添加Random_R算法

---
+++ algorithm/Random_R.py
@@ -0,0 +1,19 @@
+import deepxde as dde
+from cases.PDECases import Diffusion
+from solver.PDESolver import PINNSolver
+
+class Random_R(PINNSolver):
+    def __init__(self, PDECase, P):
+        super().__init__(name=f'Random_R_P_{P}', PDECase=PDECase)
+        self.P = P
+        self.resampler = dde.callbacks.PDEPointResampler(period=self.P)
+
+    def closure(self):
+        self.train_step(callbacks=[self.resampler])
+
+
+if __name__ == '__main__':
+    PDECase = Diffusion(NumDomain=2000)
+    solver = Random_R(P=00, PDECase=PDECase)
+    solver.train()
+    solver.save(add_time=True)
