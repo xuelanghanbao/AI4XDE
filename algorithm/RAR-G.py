110d03f0d98475a036524d5eb6f205ebee3297a2
xuelanghanbao <710616871@qq.com>
2023-04-26 20:57:54 +0800

    添加RAR-G方法

---
+++ algorithm/RAR-G.py
@@ -0,0 +1,29 @@
+import numpy as np
+import deepxde as dde
+from deepxde.backend import torch
+from cases.PDECases import AllenCahn
+from solver.PDESolver import PINNSolver
+
+dde.optimizers.config.set_LBFGS_options(maxiter=1000)
+
+class RAR_G(PINNSolver):
+    def __init__(self, PDECase):
+        super().__init__(name='RAR_G', PDECase=PDECase)
+
+    def closure(self):
+        self.train_step()
+
+        for i in range(100):
+            X = self.PDECase.geomtime.random_points(100000)
+            Y = np.abs(self.model.predict(X, operator=self.PDECase.pde))[:, 0]
+            err_eq = torch.tensor(Y)
+            X_ids = torch.topk(err_eq, self.PDECase.NumDomain//100, dim=0)[1].cpu().numpy()
+            self.PDECase.data.add_anchors(X[X_ids])
+
+            self.train_step(iterations=1000)
+
+if __name__ == '__main__':
+    PDECase = AllenCahn(NumDomain=2000//2)
+    solver = RAR_G(PDECase=PDECase)
+    solver.train()
+    solver.save(add_time=True)
