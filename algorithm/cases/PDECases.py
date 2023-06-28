615dc2703d714436838d6f2902928247314710b0
xuelanghanbao <710616871@qq.com>
2023-06-28 22:12:39 +0800

    重构画图部分代码

--- algorithm/cases/PDECases.py
+++ algorithm/cases/PDECases.py
@@ -106,30 +106,34 @@ class Burgers(PDECases):
     def output_transform(self, x, y):
         return -self.backend.sin(np.pi * x[:, 0:1]) + (1 - x[:, 0:1] ** 2) * (x[:, 1:]) * y
     
-    def plot_data(self,X):
+    def set_axes(self, axes):
+        axes.set_xlim(0, 1)
+        axes.set_ylim(-1, 1)
+        axes.set_xlabel('t')
+        axes.set_ylabel('x')
+
+    def plot_data(self, X, axes=None):
         from matplotlib import pyplot as plt
-        plt.xlim(0, 1)
-        plt.ylim(-1, 1)
-        plt.xlabel('t')
-        plt.ylabel('x')
-        plt.scatter(X[:, 1], X[:, 0])
-        plt.show()
+        if axes is None:
+            fig, axes = plt.subplots()
+        self.set_axes(axes)
+        axes.scatter(X[:, 1], X[:, 0])
+        return axes
     
     def plot_heatmap_at_axes(self, X, y, axes, title):
         axes.set_title(title)
-        axes.set_xlabel('t')
-        axes.set_ylabel('x')
+        self.set_axes(axes)
         axes.pcolormesh(X[:, 1].reshape(100, 256), X[:, 0].reshape(100, 256), y.reshape(100, 256), cmap='rainbow')
     
     def plot_heatmap(self, solver):
         from matplotlib import pyplot as plt
         X, y = self.gen_testdata()
-        fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
         
-        self.plot_heatmap_at_axes(X, y, axes=axes[0][0], title='Exact solution')
+        self.plot_heatmap_at_axes(X, y, axes=axes[0], title='Exact solution')
         model_y = solver.model.predict(X)
-        self.plot_heatmap_at_axes(X, model_y, axes[1][0], title=solver.name)
-        self.plot_heatmap_at_axes(X, np.abs(model_y - y) , axes[1][1], title='Absolute error')
+        self.plot_heatmap_at_axes(X, model_y, axes[1], title=solver.name)
+        self.plot_heatmap_at_axes(X, np.abs(model_y - y) , axes[2], title='Absolute error')
         plt.show()
         return fig, axes
     
