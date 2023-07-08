1c292bf805bce807e9adf2a0a1be4c9646f41420
xuelanghanbao <710616871@qq.com>
2023-07-08 15:34:01 +0800

    增加HPO算法

--- AI4XDE/algorithm/__init__.py
+++ AI4XDE/algorithm/__init__.py
@@ -1,17 +1,19 @@
 __all__ = [
+    "HPO",
     "PINN",
-    "Uniform",
+    "R3Sampling",
+    "RAD",
     "Random_R",
     "RAR_D",
     "RAR_G",
-    "RAD",
-    "R3Sampling",
+    "Uniform", 
 ]
 
+from .HPO import HPO
 from .PINN import PINN
-from .Uniform import Uniform
+from .R3Sampling import R3Sampling
+from .RAD import RAD
 from .Random_R import Random_R
 from .RAR_D import RAR_D
 from .RAR_G import RAR_G
-from .RAD import RAD
-from .R3Sampling import R3Sampling
+from .Uniform import Uniform
