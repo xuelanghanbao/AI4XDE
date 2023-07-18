88275e7505711fb5e207be2ded4f3909ec403606
xuelanghanbao <710616871@qq.com>
2023-07-18 16:30:29 +0800

    增加Lorenz_Exogenous_Input_Inverse样例

--- AI4XDE/cases/InverseCases.py
+++ AI4XDE/cases/InverseCases.py
@@ -125,3 +125,122 @@ class Lorenz_Inverse(InverseCase):
         print(f"C1 true: {C1_true}, C2 true: {C2_true}, C3 true: {C3_true}")
         print(f"C1 pred: {C1_pred}, C2 pred: {C2_pred}, C3 pred: {C3_pred}")
         print(f"C1 error: {C1_error}, C2 error: {C2_error}, C3 error: {C3_error}")
+
+
+class Lorenz_Exogenous_Input_Inverse(InverseCase):
+    def __init__(
+        self,
+        NumDomain=400,
+        layer_size=[1] + [40] * 3 + [3],
+        activation="tanh",
+        initializer="Glorot uniform",
+    ):
+        self.C1 = dde.Variable(1.0)
+        self.C2 = dde.Variable(1.0)
+        self.C3 = dde.Variable(1.0)
+        self.C1_true = 10
+        self.C2_true = 15
+        self.C3_true = 8 / 3
+        super().__init__(
+            "Inverse problem for the Lorenz system with exogenous input",
+            external_trainable_variables=[self.C1, self.C2, self.C3],
+            NumDomain=NumDomain,
+            use_output_transform=False,
+            layer_size=layer_size,
+            activation=activation,
+            initializer=initializer,
+        )
+
+    def gen_testdata(self):
+        import scipy as sp
+        from scipy.integrate import odeint
+
+        def ex_func(t):
+            spline = sp.interpolate.Rbf(
+                time, ex_input, function="thin_plate", smooth=0, episilon=0
+            )
+            return spline(t)
+
+        def LorezODE(x, t):
+            x1, x2, x3 = x
+            dxdt = [
+                self.C1_true * (x2 - x1),
+                x1 * (self.C2_true - x3) - x2,
+                x1 * x2 - self.C3_true * x3 + ex_func(t),
+            ]
+            return dxdt
+
+        maxtime = 3
+        time = np.linspace(0, maxtime, 200)
+        ex_input = 10 * np.sin(2 * np.pi * time)
+        x0 = [-8, 7, 27]
+        x = odeint(LorezODE, x0, time)
+        return time[:, np.newaxis], x
+
+    def gen_pde(self):
+        def Lorenz_system(x, y, ex):
+            y1, y2, y3 = y[:, 0:1], y[:, 1:2], y[:, 2:]
+            dy1_x = dde.grad.jacobian(y, x, i=0)
+            dy2_x = dde.grad.jacobian(y, x, i=1)
+            dy3_x = dde.grad.jacobian(y, x, i=2)
+            return [
+                dy1_x - self.C1 * (y2 - y1),
+                dy2_x - y1 * (self.C2 - y3) + y2,
+                dy3_x - y1 * y2 + self.C3 * y3 - ex,
+            ]
+
+        return Lorenz_system
+
+    def gen_geomtime(self):
+        return dde.geometry.TimeDomain(0, 3)
+
+    def gen_data(self):
+        ic1 = dde.icbc.IC(
+            self.geomtime, lambda X: -8, lambda _, on_initial: on_initial, component=0
+        )
+        ic2 = dde.icbc.IC(
+            self.geomtime, lambda X: 7, lambda _, on_initial: on_initial, component=1
+        )
+        ic3 = dde.icbc.IC(
+            self.geomtime, lambda X: 27, lambda _, on_initial: on_initial, component=2
+        )
+        observe_t, ob_y = self.gen_testdata()
+        observe_y0 = dde.icbc.PointSetBC(observe_t, ob_y[:, 0:1], component=0)
+        observe_y1 = dde.icbc.PointSetBC(observe_t, ob_y[:, 1:2], component=1)
+        observe_y2 = dde.icbc.PointSetBC(observe_t, ob_y[:, 2:3], component=2)
+
+        def ex_func2(t):
+            import scipy as sp
+
+            maxtime = 3
+            time = np.linspace(0, maxtime, 200)
+            ex_input = 10 * np.sin(2 * np.pi * time)
+            spline = sp.interpolate.Rbf(
+                time, ex_input, function="thin_plate", smooth=0, episilon=0
+            )
+            return spline(t[:, 0:])
+
+        return dde.data.PDE(
+            self.geomtime,
+            self.pde,
+            [ic1, ic2, ic3, observe_y0, observe_y1, observe_y2],
+            num_domain=self.NumDomain,
+            num_boundary=2,
+            anchors=observe_t,
+            auxiliary_var_function=ex_func2,
+        )
+
+    def plot_result(self, solver):
+        C1_pred = bkd.to_numpy(self.C1)
+        C2_pred = bkd.to_numpy(self.C2)
+        C3_pred = bkd.to_numpy(self.C3)
+
+        C1_error = np.abs(self.C1_true - C1_pred)
+        C2_error = np.abs(self.C2_true - C2_pred)
+        C3_error = np.abs(self.C3_true - C3_pred)
+
+        print(
+            f"C1 true: {self.C1_true}, C2 true: {self.C2_true}, C3 true: {self.C3_true}"
+        )
+        print(f"C1 pred: {C1_pred}, C2 pred: {C2_pred}, C3 pred: {C3_pred}")
+        print(f"C1 error: {C1_error}, C2 error: {C2_error}, C3 error: {C3_error}")
