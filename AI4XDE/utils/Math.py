584222fb80fd2c6856b84ddbf0a9984e0aab2603
xuelanghanbao <710616871@qq.com>
2023-08-28 19:08:14 +0800

    增加NTK计算（只支持pytorch）

--- AI4XDE/utils/Math.py
+++ AI4XDE/utils/Math.py
@@ -1,4 +1,6 @@
 import numpy as np
+import deepxde.backend as bkd
+from deepxde.backend import torch
 
 
 def transform_uniform_to_normal_2D(X):
@@ -16,3 +18,77 @@ def transform_normal_to_truncated_normal_on_geomtime(X, geomtime, mul=0, sigma=1
     X = X * sigma + mul
     X = X[geomtime.inside(X)]
     return X
+
+
+def ntk(PDECase, solver, compute="full"):
+    # TODO: support other backends
+    if bkd.backend_name != "pytorch":
+        raise RuntimeError("NTK is only supported for PyTorch >= 2.0")
+    from functorch import make_functional, vmap, jacrev
+
+    def fnet_single(params, x):
+        return fnet(params, x.unsqueeze(0)).squeeze(0)
+
+    x = PDECase.geomtime.random_points(PDECase.NumDomain)
+    x = bkd.from_numpy(x)
+
+    # pytorch>=2.0
+
+    # functorch
+    fnet, params = make_functional(solver.model.net)
+    jac1 = vmap(jacrev(fnet_single), (None, 0))(params, x)
+    jac1 = [j.flatten(2) for j in jac1]
+
+    # Compute J(x2)
+    jac2 = vmap(jacrev(fnet_single), (None, 0))(params, x)
+    jac2 = [j.flatten(2) for j in jac2]
+
+    # Compute J(x1) @ J(x2).T
+    einsum_expr = None
+    if compute == "full":
+        einsum_expr = "Naf,Mbf->NMab"
+    elif compute == "trace":
+        einsum_expr = "Naf,Maf->NM"
+    elif compute == "diagonal":
+        einsum_expr = "Naf,Maf->NMa"
+    else:
+        assert False
+
+    result = torch.stack(
+        [torch.einsum(einsum_expr, j1, j2) for j1, j2 in zip(jac1, jac2)]
+    )
+    result = result.sum(0)
+    return result.squeeze()
+
+
+def eig_of_ntk(PDECase, solver, sort=True, **kwargs):
+    ntk_result = ntk(PDECase, solver, **kwargs)
+    lambda_K, eigvec_K = np.linalg.eig(bkd.to_numpy(ntk_result))
+    if sort:
+        lambda_K = np.sort(np.real(lambda_K))[::-1]
+    return lambda_K, eigvec_K
+
+
+def visualize_ntk(PDECase, solver, **kwargs):
+    import matplotlib.pyplot as plt
+
+    lambda_K, eigvec_K = eig_of_ntk(PDECase, solver, **kwargs)
+    # Visualize the eigenvectors of the NTK
+    fig, axs = plt.subplots(2, 3, figsize=(12, 6))
+    X = np.linspace(0, 1, PDECase.NumDomain)
+    axs[0, 0].plot(X, np.real(eigvec_K[:, 0]))
+    axs[0, 1].plot(X, np.real(eigvec_K[:, 1]))
+    axs[0, 2].plot(X, np.real(eigvec_K[:, 2]))
+    axs[1, 0].plot(X, np.real(eigvec_K[:, 3]))
+    axs[1, 1].plot(X, np.real(eigvec_K[:, 4]))
+    axs[1, 2].plot(X, np.real(eigvec_K[:, 5]))
+    plt.show()
+
+    # Visualize the eigenvalues of the NTK
+    fig, ax = plt.subplots(figsize=(6, 5))
+    ax.plot(lambda_K.squeeze())
+    plt.xscale("log")
+    plt.yscale("log")
+    ax.set_xlabel("index")
+    ax.set_ylabel(r"$\lambda$")
+    plt.show()
